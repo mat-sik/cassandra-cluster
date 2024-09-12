5770eb5a3e9c5076b1491d54256e71663d7f37cf
Mateusz <msikorski@griddynamics.com>
2024-09-12 13:55:49 +0200

    Implemented basic version that can connect to the cluster.

--- src/main/java/mat_sik/cassandra/chat/Application.java
+++ src/main/java/mat_sik/cassandra/chat/Application.java
@@ -1,13 +1,39 @@
 package mat_sik.cassandra.chat;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.net.InetSocketAddress;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return _ -> {
+            try (CqlSession session = CqlSession.builder()
+                    .addContactPoints(List.of(
+                            new InetSocketAddress("localhost", 9042),
+                            new InetSocketAddress("localhost", 9043),
+                            new InetSocketAddress("localhost", 9044)
+                    ))
+                    .withLocalDatacenter("DC1")
+                    .build()) {
+                ResultSet rs = session.execute("select release_version from system.local");
+                Row row = rs.one();
+                System.out.println(row.getString("release_version"));
+            }
+        };
+    }
 
 }
