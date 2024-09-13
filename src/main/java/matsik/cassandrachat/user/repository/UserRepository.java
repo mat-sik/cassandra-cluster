fc2c4d1a5c310324f93b3262260e2b4e5cdfdbb6
Mateusz <msikorski@griddynamics.com>
2024-09-13 10:47:59 +0200

    Made use of cas transactions.

--- src/main/java/matsik/cassandrachat/user/repository/UserRepository.java
+++ src/main/java/matsik/cassandrachat/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package matsik.cassandrachat.user.repository;
 
-import com.datastax.oss.driver.api.core.cql.BoundStatement;
 import com.datastax.oss.driver.api.mapper.annotations.Dao;
 import com.datastax.oss.driver.api.mapper.annotations.Delete;
 import com.datastax.oss.driver.api.mapper.annotations.Insert;
@@ -13,9 +12,9 @@ public interface UserRepository {
     @Select
     User findByPrimaryKey(String username, String email);
 
-    @Insert
-    BoundStatement save(User user);
+    @Insert(ifNotExists = true)
+    boolean save(User user);
 
     @Delete(entityClass = User.class)
-    BoundStatement deleteByPrimaryKey(String username, String email);
+    void deleteByPrimaryKey(String username, String email);
 }
